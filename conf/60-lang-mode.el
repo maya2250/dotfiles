b81204fcac27ebd511450b13a728d1c66424ba6b
maya2250 <maya2250.dev@gmail.com>
2017-02-26 20:10:04 +0900

    c lang 周り設定したつもり

--- conf/60-lang-mode.el
+++ conf/60-lang-mode.el
@@ -1,33 +1,46 @@
+;; 左端(文字の前)ではインデント、それ以外はタブの挿入
+(setq tab-always-indent nil)
+(setq c-tab-always-indent nil)
+
 ;; C Coding style
 (defun my-c-mode-init ()
+  (hs-minor-mode 1)
+  (setq c-auto-newline t)
+  (c-set-style "bsd")
+)
+
+(defun my-sh-mode-init ()
   (hs-minor-mode 1)
   (setq c-tab-always-indent t)
   (setq c-auto-newline t)
 )
 
-(add-hook 'c-mode-hook 'my-c-mode-init)
 
-
-;
 ; Scheme coding style
-(add-hook 'scheme-mode-hook
-          '(lambda ()
-      (hs-minor-mode 1)))
+(defun my-scheme-mode-init ()
+  (hs-minor-mode 1)
+)
 
-;
 ; Elisp coding style
-(add-hook 'emacs-lisp-mode-hook
-          '(lambda ()
-      (hs-minor-mode 1)))
+(defun my-emacs-lisp-mode-init ()
+  (hs-minor-mode 1)
+)
 
-;
 ; Lisp coding style
-(add-hook 'lisp-mode-hook
-          '(lambda ()
-      (hs-minor-mode 1)))
+(defun my-lisp-mode-init ()
+  (hs-minor-mode 1)
+)
 
-;
 ; Python coding style
-(add-hook 'python-mode-hook
-          '(lambda ()
-      (hs-minor-mode 1)))
+(defun my-python-mode-init ()
+  (hs-minor-mode 1)
+)
+
+
+;; (add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-hook 'my-c-mode-init)
+(add-hook 'sh-mode-hook 'my-sh-mode-init)
+(add-hook 'scheme-mode-hook 'my-scheme-mode-init)
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-init)
+(add-hook 'lisp-mode-hook 'my-lisp-mode-init)
+(add-hook 'python-mode-hook 'my-python-mode-init)
--- conf/99-key-bind.el
+++ conf/99-key-bind.el
@@ -7,3 +7,6 @@
 ;; メタキー（ESC）とoptionキーの入れ替え
 (setq ns-command-modifier (quote meta))
 (setq ns-alternate-modifier (quote super))
+
+;; tabキー
+;; (setq global-set-key ("TAB") )
