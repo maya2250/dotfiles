8707cb1cdf152488c2a883884c05c8a9efa01bf5
maya2250 <maya2250.dev@gmail.com>
2017-03-03 09:52:43 +0900

    add various package

--- elpa/sass-mode-3.0.16/sass-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sass-mode" "3.0.16" "Major mode for editing Sass files" '((haml-mode "3.0.15")))
--- elpa/scss-mode-0.5.0/scss-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "scss-mode" "0.5.0" "Major mode for editing SCSS files" 'nil)
