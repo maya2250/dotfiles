08509a9502b33458f100ca31a0c13e6f24e63b73
maya2250 <maya2250.dev@gmail.com>
2017-03-03 17:52:06 +0900

    add web-mode-{css,code}-indent-offset

--- conf/60-lang-mode.el
+++ conf/60-lang-mode.el
@@ -21,6 +21,8 @@
 ;; web style
 (defun my-web-mode-init ()
   (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
 )
 (add-hook 'web-mode-hook 'my-web-mode-init)
 
