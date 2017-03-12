043d3679b635307309d26ea86163f6da9f0d54e0
maya2250 <maya2250.dev@gmail.com>
2017-03-12 17:19:57 +0900

    add nyan mode

--- conf/30-package.el
+++ conf/30-package.el
@@ -122,3 +122,6 @@
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
+;; ------------------------------------------------------------------------
+;; @  nyan mode
+(nyan-mode 1)
