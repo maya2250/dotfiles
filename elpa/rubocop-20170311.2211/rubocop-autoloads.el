878762c0953ef77aeb347189cd76afa09689218d
maya2250 <maya2250.dev@gmail.com>
2017-03-13 01:10:48 +0900

    modify nyan-animation

--- elpa/rubocop-20170311.2211/rubocop-autoloads.el
+++ elpa/rubocop-20170311.2211/rubocop-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "rubocop" "rubocop.el" (22705 55347 0 0))
+;;;### (autoloads nil "rubocop" "rubocop.el" (22725 26683 0 0))
 ;;; Generated autoloads from rubocop.el
 
 (autoload 'rubocop-check-project "rubocop" "\
