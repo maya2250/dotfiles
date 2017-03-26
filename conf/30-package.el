9723bba2bd47cbfcef00fe69d1254039e5927172
maya2250 <maya2250.dev@gmail.com>
2017-03-26 19:44:53 +0900

    add c++11 for flycheck

--- conf/30-package.el
+++ conf/30-package.el
@@ -170,3 +170,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (define-key global-map (kbd "C-c n") 'flycheck-next-error)
 (define-key global-map (kbd "C-c p") 'flycheck-previous-error)
+
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++11")))
+
+
