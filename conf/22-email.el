c45b318ce738baedf518d8e228a23c7f60a679d1
maya2250 <maya2250.dev@gmail.com>
2017-04-09 00:57:30 +0900

    add 22-email.el
    require modify

---
+++ conf/22-email.el
@@ -0,0 +1,9 @@
+;; ------------------------------------------------------------------------
+;; @ mail setting
+;; ------------------------------------------------------------------------
+
+(setq user-mail-address "ipt2250@gmail.com")
+(setq user-full-name "Shinya Murakami")
+(setq smtpmail-smtp-server "smtp.gmail.com")
+(setq mail-user-agent 'message-user-agent)
+(setq message-send-mail-function 'message-smtpmail-send-it)
