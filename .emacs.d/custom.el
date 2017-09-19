855032e6ef68a5f1e15258b95cd5eeb332f56356
maya2250 <maya2250.dev@gmail.com>
2017-09-19 22:16:26 +0900

    add vue-mode, vue-html-mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,7 +8,7 @@
     ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
-    (git-gutter-fringe company company-php company-shell flymake-python-pyflakes cmake-mode ac-php php-auto-yasnippets elixir-mode elixir-yasnippets flycheck-elixir swift-mode flycheck ctags yasnippet magit ruby-electric ido-vertical-mode nyan-mode hl-line+ hiwin rainbow-mode ac-html ac-html-bootstrap company-web html5-schema web-mode less-css-mode sass-mode scss-mode yaml-mode google-c-style smart-mode-line-powerline-theme smart-mode-line helm-rubygems-local helm-rubygems-org rubocop ruby-dev ruby-mode ruby-tools indent-guide helm phpcbf clang-format free-keys multiple-cursors neotree markdown-mode emmet-mode python-mode tabbar undo-tree auto-complete init-loader))))
+    (vue-html-mode vue-mode git-gutter-fringe company company-php company-shell flymake-python-pyflakes cmake-mode ac-php php-auto-yasnippets elixir-mode elixir-yasnippets flycheck-elixir swift-mode flycheck ctags yasnippet magit ruby-electric ido-vertical-mode nyan-mode hl-line+ hiwin rainbow-mode ac-html ac-html-bootstrap company-web html5-schema web-mode less-css-mode sass-mode scss-mode yaml-mode google-c-style smart-mode-line-powerline-theme smart-mode-line helm-rubygems-local helm-rubygems-org rubocop ruby-dev ruby-mode ruby-tools indent-guide helm phpcbf clang-format free-keys multiple-cursors neotree markdown-mode emmet-mode python-mode tabbar undo-tree auto-complete init-loader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ .emacs.d/elpa/vue-mode-20170918.2006/vue-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vue-mode" "20170918.2006" "Major mode for vue component based on mmm-mode" '((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1") (edit-indirect "0.1.4")) :commit "146c17633005fbd559cb72c911b3fd49c9a430c4" :keywords '("languages"))
