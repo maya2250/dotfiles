3f7963e31419bc7a905531cae2e898f654e06c1d
maya2250 <maya2250.dev@gmail.com>
2019-04-06 17:40:06 +0900

    install markdown-mode and setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -120,3 +120,10 @@
 (use-package mozc
   :init
   (setq default-input-method "japanese-mozc"))
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
