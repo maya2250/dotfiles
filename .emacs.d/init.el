b721110855b949375767107db22598413327c05e
maya2250 <maya2250.dev@gmail.com>
2019-05-01 19:50:32 +0900

    fix flycheck error
    - the following functions might not be defined at runtime: ...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -100,7 +100,7 @@
 
 (use-package pipenv
   :hook (python-mode . pipenv-mode)
-  :init
+  :defer
   (setq pipenv-projectile-after-switch-function
         #'pipenv-projectile-after-switch-extended))
 
