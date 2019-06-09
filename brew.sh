c20e32f90fd849587399e2a201cfa459637d04d5
maya2250 <maya2250.dev@gmail.com>
2019-06-09 18:02:20 +0900

    remove brewcasklist.txt and install script

--- brew.sh
+++ brew.sh
@@ -1,20 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # move to directory existing this shell-script
 cd $(dirname $0)
 
 # store packages to variable
 brew_list=$(cat "brewlist.txt")
-brew_cask_list=$(cat "brewcasklist.txt")
 
 # install packages
 for package in ${brew_list[@]}; do
     brew install ${package}
 done
-
-for package in ${brew_cask_list[@]}; do
-    brew cask install ${package}
-done
-
-# delete variables
-unset brew_list brew_cask_list package
