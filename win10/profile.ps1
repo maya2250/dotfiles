1096c3242ab727c6caf8600e5d0ddae7ab68473a
maya2250 <maya2250.dev@gmail.com>
2019-10-12 07:52:21 +0900

    add Chocolatey profile

--- win10/profile.ps1
+++ win10/profile.ps1
@@ -1,1 +1,7 @@
 Set-PSReadLineOption -EditMode Emacs
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
